170e6a7355e867a5149c69f97da4c3ee6597565e
Paninap <92017318+sensou-bot@users.noreply.github.com>
2024-01-31 00:08:41 +0800

    Create sum_of_ArrayValues.js

---
+++ sum_of_ArrayValues.js
@@ -0,0 +1,7 @@
+let nums = [10,20,30];
+let sum = 0; 
+
+  for (let i = 0; i<nums.length; i++){
+    sum += nums[i];
+  }
+console.log(`Sum is ${sum} of the values within the array consisting of ${nums}`); 
