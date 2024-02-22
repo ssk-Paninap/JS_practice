d39993d58cc19e83a8cd76fc237baf2d20caa6aa
Paninap <92017318+ssk-Paninap@users.noreply.github.com>
2024-02-23 06:54:43 +0800

    Create ternaryOP.js

---
+++ ternaryOP.js
@@ -0,0 +1,8 @@
+function showGreet (){
+    let a = document.getElementById("myh3");
+    let b = document.getElementById("rad1");
+    let c = document.getElementById("rad2");
+
+  //not a good example but still sampe on what a ternary operator is
+    let male = rad1.checked ? a.textContent = `You are a man` : a.textContent =`You ain't a man`;
+}
