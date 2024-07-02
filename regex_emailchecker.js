51a3243c87a7a20f5734a0e2bec7c822e4294ec8
Paninap <92017318+ssk-Paninap@users.noreply.github.com>
2024-07-02 13:37:41 +0800

    Update regex_emailchecker.js

--- regex_emailchecker.js
+++ regex_emailchecker.js
@@ -1,4 +1,7 @@
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+// /^[^/s@] = before @ there must be other chararacters @[^/s@] == after @ there must be other thing
+// \.[^/s@]+$/ = after a period there must be a char
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
     if (emailPattern.test(email.value)) {
         alert('Valid email')
     } else {
