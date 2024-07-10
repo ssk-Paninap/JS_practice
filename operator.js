59db90c602532c5d5cfcd723f6a5d0389cbc1231
Paninap <92017318+ssk-Paninap@users.noreply.github.com>
2024-07-10 17:50:25 +0800

    Create operator.js

---
+++ operator.js
@@ -0,0 +1,19 @@
+let x = 5
+let y = 3
+
+// Left shift: Shifts x's bits y positions to the left
+//let z = x<<y
+
+// Bitwise AND assignment: Performs AND operation and assigns result to x
+//let z = x&=y
+
+// Bitwise XOR assignment: Performs XOR operation and assigns result to x
+//let z = x^=y 
+
+// Logical AND assignment: Assigns y to x if x is truthy
+//let z = x&&=y
+
+// Logical OR assignment: Assigns y to z if z is falsy
+//let z = y||=z
+
+console.log(z)
